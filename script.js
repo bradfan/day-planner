b36a72f95343ea1407b57907ba70010926e1d73f
jcerb <brt.jst@gmail.com>
2021-03-07 16:03:31 -0700

    define array color progress on storage

--- script.js
+++ script.js
@@ -1,50 +1,60 @@
-var date = moment().format("dddd, MMMM Do YYYY, HH")
+var date = moment().format("dddd, MMMM Do YYYY, HHmm")
 var currentDay = document.getElementById("currentDay"); currentDay.textContent=date;
 currentDay.textContent=date
 // set var to change color depending on the hour of day
-var currentHour = +moment().format("HH");
+var currentHour = (+moment().format("HH")*100);
 console.log(currentHour)
-var $timeBlock = $("#time-block");
-var time = $(".hour").text();
-console.log(time);
-var arrayTime = time.split(":")
-
-console.log(arrayTime);
-var hour = parseInt(arrayTime)
-console.log(arrayTime)
-
-// for loop appends textarea 
-for(let i=9; i<=17; i++){
-    $timeBlock.append(timeBlock(i));
+var $timeBlock = $("#time-block")
+var time = [$(".hour").text()];
+// var time = document.querySelector(".hour")
+ console.log(time);
+var hour = parseInt[time];
+for (let i = 0; i < 8; i++) {
+    $timeBlock.append(buildTimeBlock(hour))
 }
-// function assigns color to textarea past, present,future
-function timeBlock(hour){
+// have to get this to talk to $timeBlock NOT MUCH MORE THAN PSUEDO-CODE
+function buildTimeBlock(hour){
     var background;
     if(currentHour===hour){
-        background = "present";
+        // SHOT IN THE DARK OPTION
+        $timeBlock.addClass("present")
     }else if(currentHour < hour){
         background = "past";
     }else {
         background = "future"
-    }return $timeBlock
-}
+    }
+} 
+ buildTimeBlock()
+// console.log(buildTimeBlock)
+
+// DRAWN FROM LOCAL STORAGE LESSON 4-22
+
+// (html element listening for click on save button)
+// $saveBtn.addEventListener("click", function(event){
+//     event.preventDefault();
+//     var note = document.querySelectorAll(".description").value;
+//     localStorage.setItem("description",JSON.strigify(description));
+    // localStorage.getItem("description", description);
+//     console.log(localStorage);
+// })
+
+// WORKING WITH discord GROUP 3/7
+var saveButton = document.querySelectorAll(".saveBtn");
+var description = document.querySelectorAll(".description")
+// return $timeBlock.append($hour, $textArea, $button);
+
+
+$("saveBtn").on("click", function(e){
+    // should prevents refresh
+e.preventDefault();
+var description = $document.querySelectorAll(".description").textContent;
+// local storage key JSON should parse input
+localStorage.setItem("description", JSON.stringify(description));
+localStorage.getItem("description", description);
+console.log(localStorage);
+});
+ 
 
 
 
 
-
-
-
-
-
-
-
-// // var a = "9:00"
-// var b = "12:00"
-// var arrayA = a.split(":")  //this splits the string into an array by the colon, so it will be this: ["9", "00"]
-// var numberA = parseInt( arrayA[0] )  //parseInt runs on the first element of our array, which is "9"
-
-// if(numberA < 8){
-//   numberA += 12;
-//   //any numbers that are lower than 8 from 8:00 AM, we'll add 12 to so that our PM times are on the 24-hr clock
-// }
