9341bafcd21e6c13eec28205350ff2a261a20db9
jcerb <brt.jst@gmail.com>
2021-03-08 15:19:35 -0700

    adjust local storage

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ var time = [$(".hour").text()];
 // var time = document.querySelector(".hour")
  console.log(time);
 var hour = parseInt[time];
-for (let i = 0; i < 8; i++) {
-    $timeBlock.append(buildTimeBlock(hour))
+for (let i = 0; i < 9; i++) {
+    $timeBlock.append(buildTimeBlock)
+
 }
+
+
+// append($(timeblock).addClass("past present future"));
+
 // have to get this to talk to $timeBlock NOT MUCH MORE THAN PSUEDO-CODE
 function buildTimeBlock(hour){
     var background;
@@ -28,20 +33,24 @@ function buildTimeBlock(hour){
 // console.log(buildTimeBlock)
 
 // DRAWN FROM LOCAL STORAGE LESSON 4-22
-var saveBtn = document.querySelector(".saveBtn");
+var saveBtn = $(".saveBtn");
 // (html element listening for click on save button)
-saveBtn.addEventListener("click", function(event){
+saveBtn.on("click", function(event){
     // (should prevent refresh)
     event.preventDefault();
     // (establishes "note" as a var recieving info(?) from description class and its value)
-    var description = document.querySelectorAll(".description").value;
-    var description = localStorage.getItem("description")
+    var description = $(this).siblings(".description").val()
+    //console.log("test: ", test)
+    //var description = document.querySelectorAll(".description").textContent;
+    // var description = localStorage.getItem("description")
     // (should set and get data for local storage in a string)
-    localStorage.setItem("description",description)
-    localStorage.getItem("description", description);
+    localStorage.setItem("descriptionText", description);
+    localStorage.getItem("descriptionText", description);
     console.log(localStorage)
-    // "description" is null
+    // "description" is null but "define" text and add either .text or (text)
  })   
+
+ // target by id and class description,  set the value of that area after grabbing from local storage
 
 
  
